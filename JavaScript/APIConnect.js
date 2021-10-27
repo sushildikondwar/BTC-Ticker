7415e73550738e8dd16afab05f1515c8c46a90c4
Sushil Dikondwar <sushildikondwar@gmail.com>
2021-10-27 18:53:42 +0530

    %dayChange column added with colored High/Low Comparision indicator

--- JavaScript/APIConnect.js
+++ JavaScript/APIConnect.js
@@ -11,6 +11,7 @@ fetch('https://api2.binance.com/api/v3/ticker/24hr').then((rawData) => rawData.j
             let highprice = document.createTextNode(data[index].highPrice);
             let lastprice = document.createTextNode(data[index].lastPrice);
             let volume = document.createTextNode(data[index].volume);
+            let dayChangePercent = document.createTextNode(data[index].priceChangePercent)
             let newRow = tableID.insertRow(-1);
             newRow.insertCell(0).appendChild(serial);
             newRow.insertCell(1).appendChild(symbol);
@@ -18,10 +19,12 @@ fetch('https://api2.binance.com/api/v3/ticker/24hr').then((rawData) => rawData.j
             newRow.insertCell(3).appendChild(lowprice);
             newRow.insertCell(4).appendChild(volume);
             newRow.insertCell(5).appendChild(lastprice);
+            newRow.insertCell(6).appendChild(dayChangePercent);
         }
         index++;
     }
 });
+
 setInterval(() => {
     fetch('https://api2.binance.com/api/v3/ticker/24hr').then((rawData) => rawData.json()).then((data) => {
         counter = counter + 1;
@@ -29,29 +32,39 @@ setInterval(() => {
         let tableID = document.getElementById("tableData");
         let index = 0;
         while (index < 100) {
-            if (data[index].volume != 0.00000000) {
                 tableID.rows[index + 1].cells[1].innerHTML = data[index].symbol;
                 tableID.rows[index + 1].cells[2].innerHTML = data[index].highPrice;
                 tableID.rows[index + 1].cells[3].innerHTML = data[index].lowPrice;
-                
+
                 if (tableID.rows[index + 1].cells[4].innerHTML < data[index].volume) {
-                    tableID.rows[index + 1].cells[4].innerHTML = data[index].volume;
                     tableID.rows[index + 1].cells[4].style.color = "green";
+                    tableID.rows[index + 1].cells[4].innerHTML = data[index].volume;
                 }
                 else {
-                    tableID.rows[index + 1].cells[4].innerHTML = data[index].volume;
                     tableID.rows[index + 1].cells[4].style.color = "red";
+                    tableID.rows[index + 1].cells[4].innerHTML = data[index].volume;
                 }
 
                 if (tableID.rows[index + 1].cells[5].innerHTML < data[index].lastPrice) {
-                    tableID.rows[index + 1].cells[5].innerHTML = data[index].lastPrice;
                     tableID.rows[index + 1].cells[5].style.color = "green";
+                    tableID.rows[index + 1].cells[5].innerHTML = data[index].lastPrice;
                 }
                 else {
-                    tableID.rows[index + 1].cells[5].innerHTML = data[index].lastPrice;
                     tableID.rows[index + 1].cells[5].style.color = "red";
+                    tableID.rows[index + 1].cells[5].innerHTML = data[index].lastPrice;
                 }
-            }
+
+                if (data[index].priceChangePercent < 0) {
+                    tableID.rows[index+1].cells[6].style.color = "red";
+                    tableID.rows[index+1].cells[6].innerHTML = data[index].priceChangePercent;
+                }
+                else if(data[index].priceChangePercent > 0){
+                    tableID.rows[index+1].cells[6].style.color = "green";
+                    tableID.rows[index+1].cells[6].innerHTML = data[index].priceChangePercent;
+                }
+                else {
+                    tableID.rows[index+1].cells[6].innerHTML = data[index].priceChangePercent;
+                }
             index++;
         }
     }).catch((err) => {
